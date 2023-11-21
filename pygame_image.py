2b4b87d27a9a73108115727767c9d22a935f994c
akira <c0b220238a@edu.teu.ac.jp>
2023-11-21 17:04:52 +0900

    演習完成

--- pygame_image.py
+++ pygame_image.py
@@ -1,31 +1,34 @@
 import sys
 import pygame as pg
 
-
 def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))#練習1
     clock  = pg.time.Clock()
-    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")#練習1
     
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")#練習1
     kk_img = pg.image.load("ex01/fig/3.png")#練習2
+    bgs_img = pg.transform.flip(bg_img, True, False)
     kk_img = pg.transform.flip(kk_img, True, False)##練習2左右反転
     kk_imgs = [kk_img,pg.transform.rotozoom(kk_img, 5, 1.0)]##練習3、10°回転
 
 
     tmr = 0
+    a = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
         x = tmr%1600 
         screen.blit(bg_img, [-x, 0])
-        screen.blit(bg_img, [1600-x, 0])
-        screen.blit(kk_imgs[tmr%2],[300, 200])
-        
-        pg.display.update()
+        screen.blit(bgs_img,[1600-x, 0])
+        screen.blit(kk_imgs[a%2],[300, 200])
 
-        tmr += 1       
+
+        pg.display.update()
+        if tmr % 15 == 0:
+            a += 1     
+        tmr += 1     
         clock.tick(100)
 
 
